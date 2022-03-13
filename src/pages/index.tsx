863ab514768ae63ef1cf197dc920ab2e0e37f1d6
Italo Cedro <italo.cedro.sales@gmail.com>
2022-03-12 23:34:22 -0300

    Add document file and google fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
+import Head from "next/head";
 import styles from "../styles/home.module.scss";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Hello World</h1>
-    </div>
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+
+      <div className={styles.container}>
+        <h1 className={styles.title}>Hello World</h1>
+      </div>
+    </>
   );
 }
